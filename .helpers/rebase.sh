cf11eefd441f007c774ad45b780218749c61dbef
Kevin Collas-Arundell <me@kca.id.au>
2020-05-01 20:22:25 +1000

    update rebase script

--- .helpers/rebase.sh
+++ .helpers/rebase.sh
@@ -4,8 +4,7 @@
 # this exists to help me do bug fixes as I work on posts in the series
 
 upstream=0-gke-and-spinnaker
-for branch in 1-project-setup  2-iam-for-days  3-gke-module
-do
-echo $upstream $branch
-git rebase -v $upstream $branch
+for branch in 1-project-setup  2-gke-module 3-worker-clusters 4-iam-for-days; do
+    echo $upstream $branch
+    git rebase -v $upstream $branch
 done
